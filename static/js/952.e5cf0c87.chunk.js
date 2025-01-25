57ce80b2adfb0070a42aeed1155c56beae262202
rptux <100ciencias@gmail.com>
2025-01-25 15:02:20 -0300

    Updates

---
+++ static/js/952.e5cf0c87.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrespire_equilibre_app=self.webpackChunkrespire_equilibre_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.e5cf0c87.chunk.js.map
